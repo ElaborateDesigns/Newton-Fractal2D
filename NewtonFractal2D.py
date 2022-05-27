0d94b07663e5a2c7e2e39bee8953051b942b7718
SeDe-cmd <seth.edenkrantz@gmail.com>
2022-05-27 12:41:23 +0200

    Update NewtonFractal2D.py

--- NewtonFractal2D.py
+++ NewtonFractal2D.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as col
 import sys
 
+
+
+class Zeros:
+    def __init__(self,zero):
+        self.zero = zero
+    
 class fractal2D:
-    def __init__(self, pol1, pol2, nrIterations = 10):
+    def __init__(self, pol1, pol2, nrIterations = 10, err = 1e-100):
         self.pol1 = pol1
         self.pol2 = pol2
         self.nrIterations = nrIterations
+        self.err = err
 
     def newtonsMethod(self, guess):
         pol1 = self.pol1
@@ -51,29 +58,60 @@ class fractal2D:
             jMnumbers[1,1] = jacobianMatrix[1,1](guess[0],guess[1])
 
             #print(jMnumbers)
+            if np.linalg.det(jMnumbers) == 0:
+                print("lmao")
+                jMnumbers[0,0] = jMnumbers[0,0] + self.err
+                jMnumbers[1,1] = jMnumbers[0,1] + self.err
             jMnumbers = np.linalg.inv(jMnumbers)
             
             dotProduct = np.dot(jMnumbers,funMatrix)
             guess = guess - dotProduct #xn-1 = xn - J^-1*f(xn)
             gList.append(guess)
-            err = 1e-100
             if len(gList) > 2:
-                if abs(gList[i][0] - gList[i-1][0]) < err and abs(gList[i][1] - gList[i-1][1]) < err:
+                if abs(gList[i][0] - gList[i-1][0]) < self.err and abs(gList[i][1] - gList[i-1][1]) < self.err:
                     print(guess)
                     return guess
 
-            print(i)
+            #print(i)
         
 
         print(guess)
         return guess
-    def Guesszeri(self,xd):
-            pass
-
-
-
+    def getAmountZeros(self,x0):
+        Zeros=[]
+        if len(x0) > 2:
+            for i in range(len(x0)):
+                q = self.newtonsMethod(x0[i])
+                if len(Zeros) >= 1:
+                    for c in range(len(Zeros)):
+                        t,u = abs(q[0] - Zeros[c][0]),abs(q[1] - Zeros[c][1])
+                        #print(f"This is {t}")
+                        if t < self.err and u < self.err:
+                            print("shit")
+                            pass
+                else:
+                    Zeros.append(q)
+        else:
+            print("sjhit")
+            q = self.newtonsMethod(x0)
+            Zeros.append(q)
+        
+        #print(Zeros)
+        print(len(Zeros))
+        return len(Zeros)
 
 a = sym.Symbol('a')
 b = sym.Symbol('b')
-p = fractal2D(a**3 - 3*a*b**2 - 2, 3*a**2*b, 1000)
-fractal2D.newtonsMethod(p,[2,3])
+p = fractal2D(a**3 - 3*a*b**2 - 2*a - 2, 3*a**2*b- b**3 - 2*b, 1000)
+
+
+
+
+tlist=[]
+for c in range(100):
+    iList = [c,c+1]
+    tlist.append(iList)
+print(tlist)
+
+q = [0,1]
+fractal2D.getAmountZeros(p,tlist)
